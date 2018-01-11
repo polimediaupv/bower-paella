01f123c4a74ff4ed5644009dc0099fcc56f59c4e
Polimedia Team <polim@upvnet.upv.es>
2018-01-11 12:57:19 +0000

    [automated publishing] version 5.2.9-beta18

--- build/player/javascript/paella_player.js
+++ build/player/javascript/paella_player.js
@@ -5,7 +5,7 @@ var GlobalParams = {
 };
 var paella = {};
 paella.player = null;
-paella.version = "5.2.9 - build: aaa0df3";
+paella.version = "5.2.9 - build: 95b089a";
 paella.events = {
   play: "paella:play",
   pause: "paella:pause",
@@ -11470,47 +11470,6 @@ new (Class(paella.userTracking.SaverPlugIn, {
 }))();
 
 "use strict";
-new (Class(paella.userTracking.SaverPlugIn, {
-  getName: function() {
-    return "es.upv.paella.usertracking.GoogleAnalyticsSaverPlugin";
-  },
-  checkEnabled: function(onSuccess) {
-    var trackingID = this.config.trackingID;
-    var domain = this.config.domain || "auto";
-    if (trackingID) {
-      base.log.debug("Google Analitycs Enabled");
-      (function(i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function() {
-          (i[r].q = i[r].q || []).push(arguments);
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o), m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m);
-      })(window, document, 'script', '//www.google-analytics.com/analytics.js', '__gaTracker');
-      __gaTracker('create', trackingID, domain);
-      __gaTracker('send', 'pageview');
-      onSuccess(true);
-    } else {
-      base.log.debug("No Google Tracking ID found in config file. Disabling Google Analitycs PlugIn");
-      onSuccess(false);
-    }
-  },
-  log: function(event, params) {
-    if ((this.config.category === undefined) || (this.config.category === true)) {
-      var category = this.config.category || "PaellaPlayer";
-      var action = event;
-      var label = "";
-      try {
-        label = JSON.stringify(params);
-      } catch (e) {}
-      __gaTracker('send', 'event', category, action, label);
-    }
-  }
-}))();
-
-"use strict";
 function buildVideo360Canvas(stream, canvas) {
   var Video360Canvas = function($__super) {
     function Video360Canvas(stream) {
@@ -11936,6 +11895,47 @@ Class("paella.videoFactories.Video360Factory", {
     return new paella.Video360(id, streamData, rect.x, rect.y, rect.w, rect.h);
   }
 });
+
+"use strict";
+new (Class(paella.userTracking.SaverPlugIn, {
+  getName: function() {
+    return "es.upv.paella.usertracking.GoogleAnalyticsSaverPlugin";
+  },
+  checkEnabled: function(onSuccess) {
+    var trackingID = this.config.trackingID;
+    var domain = this.config.domain || "auto";
+    if (trackingID) {
+      base.log.debug("Google Analitycs Enabled");
+      (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+          (i[r].q = i[r].q || []).push(arguments);
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m);
+      })(window, document, 'script', '//www.google-analytics.com/analytics.js', '__gaTracker');
+      __gaTracker('create', trackingID, domain);
+      __gaTracker('send', 'pageview');
+      onSuccess(true);
+    } else {
+      base.log.debug("No Google Tracking ID found in config file. Disabling Google Analitycs PlugIn");
+      onSuccess(false);
+    }
+  },
+  log: function(event, params) {
+    if ((this.config.category === undefined) || (this.config.category === true)) {
+      var category = this.config.category || "PaellaPlayer";
+      var action = event;
+      var label = "";
+      try {
+        label = JSON.stringify(params);
+      } catch (e) {}
+      __gaTracker('send', 'event', category, action, label);
+    }
+  }
+}))();
 
 "use strict";
 function buildVideo360ThetaCanvas(stream, canvas) {
